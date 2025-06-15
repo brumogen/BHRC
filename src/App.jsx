2a95d3ff4e3b53bc091ba2ad3f51772e0bc60f8a
brumogen <shanesalmon@hotmail.co.uk>
2025-06-15 21:35:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+  return <h1>Hello from App!</h1>;
 }
