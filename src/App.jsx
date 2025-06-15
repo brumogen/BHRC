8ad817b05a8c09a076a48e4bda308f3bd39e5cda
brumogen <shanesalmon@hotmail.co.uk>
2025-06-15 22:27:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-function Home() {
-  return <h1>Home Page Test</h1>;
-}
+import Navbar from './components/Navbar'; // Make sure this exists
+import Home from './pages/Home';
+import Mint from './pages/Mint';
+import Stable from './pages/Stable';
+import RaceCentre from './pages/RaceCentre';
+import About from './pages/About';
 
 export default function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/mint" element={<Mint />} />
+        <Route path="/stable" element={<Stable />} />
+        <Route path="/race-centre" element={<RaceCentre />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
