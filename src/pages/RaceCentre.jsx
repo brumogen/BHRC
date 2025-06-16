eeba9442da6a3334f67d3b8b9f49f012af4430b6
brumogen <shanesalmon@hotmail.co.uk>
2025-06-16 19:02:52 +0100

    Update RaceCentre.jsx

--- src/pages/RaceCentre.jsx
+++ src/pages/RaceCentre.jsx
@@ -1,1 +1,10 @@
-<Route path="/race-centre" element={<RaceCentre />} />
+const RaceCentre = () => {
+  return (
+    <div>
+      <h1>Race Centre</h1>
+      {/* Your content */}
+    </div>
+  );
+};
+
+export default RaceCentre;
